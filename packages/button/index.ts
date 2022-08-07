9570934344d66c68589c9b8ca785d4c12ee9c017
k0a1a <thlz998@gmail.com>
2022-08-07 23:45:58 +0800

    格式化

--- packages/button/index.ts
+++ packages/button/index.ts
@@ -2,8 +2,8 @@ import App from "vue";
 import Button from "./src/button.vue";
 // 定义 install 方法， App 作为参数
 Button.install = (app: App): void => {
-  console.log(app);
-  //   app.component(Button.name, Button);
+    console.log(app);
+    //   app.component(Button.name, Button);
 };
 
 // const asda = Button
--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,12 +1,15 @@
 <template>
-  <button class="nd-btn">
-    <span v-if="$slots.default">12312<slot></slot></span>
-  </button>
+    <button class="nd-btn">
+        <span v-if="$slots.default">
+            12312
+            <slot></slot>
+        </span>
+    </button>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
-  name: "nd-button",
+    name: "nd-button",
 });
 </script>
 <style scoped></style>
